f1c65fb0164b2a136167b0c89fef693d2be83656
bamcmanus <brent.mcmanus@me.com>
2025-04-22 08:13:09 -0700

    implement fps

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 BLACK = (0, 0, 0)
+FPS = 60
 
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -19,6 +22,8 @@ def main():
                 return
         screen.fill(BLACK)
         pygame.display.flip()
+        time_elapsed = clock.tick(FPS)
+        dt = time_elapsed/1000
 
 
 if __name__ == "__main__":
